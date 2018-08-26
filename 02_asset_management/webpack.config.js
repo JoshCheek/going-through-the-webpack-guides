51000654e032bac8a1634b69efb71454e8bffd97
Josh Cheek <josh.cheek@gmail.com>
2018-08-26 13:17:54 -0500

    Load CSS (IDK why we need 2 loaders)

--- 02_asset_management/webpack.config.js
+++ 02_asset_management/webpack.config.js
@@ -2,9 +2,15 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
   }
 };
-
