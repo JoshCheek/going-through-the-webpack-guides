21b7dab7073f641dc2140c4c661c56410ee5cbc9
Josh Cheek <josh.cheek@gmail.com>
2018-08-26 15:02:16 -0500

    Add a dev server to recompile

--- 04_development/README.md
+++ 04_development/README.md
@@ -8,8 +8,19 @@ Based on the example at https://webpack.js.org/guides/development/
 $ npm run build
 ```
 
+Source Maps
+-----------
+
 An erorr without source maps:
 ![error without source maps](./without.png)
 
 And with source maps:
 ![error with source maps](./with.png)
+
+
+Responding to change
+--------------------
+
+* You can watch files and recompile when they are modified
+* `webpack-dev-server` starts a simple web server and the ability to use live reloading.
+* You can control the server instead of using `webpack-dev-server`'s (shown here)
--- 04_development/webpack.config.js
+++ 04_development/webpack.config.js
@@ -1,25 +1,26 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
+const path               = require('path')
+const HtmlWebpackPlugin  = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const ManifestPlugin     = require('webpack-manifest-plugin')
 
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js',
+    app:   './src/index.js',  // first entry point
+    print: './src/print.js',  // second entry point
   },
 
-  devtool: 'inline-source-map',
+  devtool: 'inline-source-map', // add source maps
 
   plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({title: 'Output Management'}),
-    new ManifestPlugin(),
+    new CleanWebpackPlugin(['dist']),                     // delete previously generated files from the dist dir
+    new HtmlWebpackPlugin({title: 'Output Management'}),  // generate index.html
+    new ManifestPlugin(),                                 // emit dist/manifest.json
   ],
 
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].bundle.js',          // how to name files
+    path: path.resolve(__dirname, 'dist'), // where to save files
+    publicPath: '/',                       // root path for the dev server
   },
 
-};
+}
