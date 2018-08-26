71856a44f4bb12396ba468c19e769dee543ec49b
Josh Cheek <josh.cheek@gmail.com>
2018-08-26 11:42:15 -0500

    Going through https://webpack.js.org/guides/getting-started/
    In the HTML file, I had to use "bundle.js" instead of "main.js",
    not sure what gives it the default name, maybe this changed?

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import bar from './bar'
-bar()
+import _ from 'lodash';
+
+function component() {
+  let element = document.createElement('div');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  return element;
+}
+
+document.body.appendChild(component());
