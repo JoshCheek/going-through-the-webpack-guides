f7fc42eb20ea40b011b4ce917e4f2f289a66f887
Josh Cheek <josh.cheek@gmail.com>
2018-08-26 14:02:08 -0500

    Load some JSON

--- 02_asset_management/src/index.js
+++ 02_asset_management/src/index.js
@@ -1,6 +1,9 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon2.png';
+import data from './example.json';
+
+window.j = data
 
 function component() {
   let element = document.createElement('div');
@@ -10,8 +13,12 @@ function component() {
   // Add the image to our existing div.
   var myIcon = new Image();
   myIcon.src = Icon;
-
   element.appendChild(myIcon);
+
+  // load some JSON
+  var myJson = document.createElement('pre');
+  myJson.innerText = JSON.stringify(data, null, 2);
+  element.appendChild(myJson);
 
   return element;
 }
